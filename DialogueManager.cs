815b88ae75720e0e835664a46e4d49e3acb94cfb
moonew <55537508+moonew@users.noreply.github.com>
2019-09-19 19:55:20 +0900

    Add files via upload

---
+++ DialogueManager.cs
@@ -0,0 +1,418 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    //싱글톤
+    private static DialogueManager instance;
+    public static DialogueManager GetInstance()
+    {
+        if(instance==null)
+        {
+            instance = FindObjectOfType<DialogueManager>();
+            if(instance ==null)
+            {
+                GameObject container = new GameObject("DialogueManager");
+                instance = container.AddComponent<DialogueManager>();
+            }
+        }
+        return instance;
+    }
+
+
+    //UI
+    public  GameObject     dialogueBox;
+    public  Text           dialogueText;
+    public  Text           nameText;
+
+
+    //대화창 옵션
+    float typingSpeed = 0.005f;
+    bool isClickMode = true;
+
+
+
+
+    //대사를 나눠서 출력할 것인가?
+    bool isConnectingSentence = false;
+
+
+    //코루틴
+    bool isRunningTalkCoroutine = false;
+    
+   
+
+    //대화스크립트를 담을 리스트
+    List<string> sentence = new List<string>();
+    int sentenceIndex = 1;
+
+
+
+
+
+
+
+    void SettingDialogueOption()
+    {
+        typingSpeed = PlayerPrefs.GetFloat("customTypingSpeed",0.005f);
+
+        if (PlayerPrefs.GetInt("isClickMode", 0)==0)
+        {
+            isClickMode = true;
+        }
+        else
+        {
+            isClickMode = false;
+        }
+    }
+
+
+
+
+    public void GetDialogue(string _path)
+    {
+        sentence.Clear();
+        TextAsset data = Resources.Load<TextAsset>("Script/"+_path);
+
+        if (data==null)
+        {
+            Debug.Log("경로에 파일이 없습니다.");
+            return;
+        }
+        else
+        {
+            Debug.Log(_path + "파일이 정상적으로 로드됨");
+        }
+
+        string dialogueData = data.ToString();
+
+        
+        string[] container = dialogueData.Split('\n');
+        foreach(string s in container)
+        {
+            sentence.Add(s);
+        }
+        
+    }
+
+
+
+    #region 스크립팅 관련
+    public void Talk(float _targetTime = 0.005f)
+    {
+        StartCoroutine("TalkCoroutine", _targetTime);
+    }
+
+    public void Talk()
+    {
+        typingSpeed = PlayerPrefs.GetFloat("customTypingSpeed", 0.005f);
+        StartCoroutine("TalkCoroutine", typingSpeed);
+    }
+    IEnumerator TalkCoroutine(float _targetTime)
+    {
+
+        //코루틴 처리되는 동안 중복 클릭을 막기 위한 변수
+        isRunningTalkCoroutine = true;
+
+
+      
+
+        if (sentenceIndex == sentence.Count - 1)
+        {
+            Debug.Log(sentence[sentenceIndex]);
+            Debug.Log("대화 끝남");
+            yield break;
+        }
+
+        string[] element = sentence[sentenceIndex].Split(',');
+
+        Debug.Log("이름 : " + element[1]);
+        Debug.Log("대사 : " + element[2]);
+
+        //눌 체크 
+
+        //명령어가 입력되어 있을 때.
+        if(element.Length>3)
+        {
+            string[] command = element[3].Split(' ');
+            foreach(string s in command)
+            {
+                ScriptCommand(s);
+            }
+        }
+
+
+
+
+        //이름이 입력되어있으면
+        if (element[1] != "")
+        {
+            //그대로 입력된 이름이 출력된다.
+           nameText.text = element[1];
+        }
+        //이름이 입력되어있지 않으면
+        else
+        {
+            //이름과 대사가 모두 입력되어 있지 않을때는
+            if(element[2] == "")
+            {
+                //이름창을 비운다. 
+                nameText.text = "";
+            }
+            //이름은 없지만 대사가 입력되어있는 경우에는 바로 전 화자의 이름이 출력된다.
+        }
+
+        //문자연결모드가 on이면 대화창을 비우지 않고 전 문장에 바로 이어서 작성한다.
+        if (isConnectingSentence == false)
+        {
+            dialogueText.text = "";
+        }
+        else
+        {
+            dialogueText.text += " ";
+        }
+
+        //타이핑 코루틴이 출력된다.
+        yield return StartCoroutine(Typing(element[2], _targetTime));
+
+        ++sentenceIndex;
+
+        //중복클릭방지 잠금을 해제하여 클릭이 가능하도록 한다.
+        isRunningTalkCoroutine = false; 
+        yield return null;
+        
+    }
+
+    IEnumerator Typing(string _sentence ,float _targetTime)
+    {
+        //캐릭터가 그려지는 동안에는 대화가 시작되지 않는다.
+        while (SpriteManager.GetInstance().isDrawingCharacter == true)
+        {
+            yield return null;
+        }
+
+        string script = _sentence;
+        foreach(char s in script)
+        {
+            dialogueText.text += s;
+            yield return new WaitForSeconds(_targetTime);
+        }
+    }
+    
+    public void OpenDialogueBox(bool _toggle)
+    {
+        if(_toggle==true)
+        {
+            dialogueBox.GetComponent<CanvasGroup>().alpha = 1;
+        }
+        else
+        {
+            dialogueBox.GetComponent<CanvasGroup>().alpha = 0;
+        }
+    }
+
+    //명령어
+    void ScriptCommand(string _command)
+    {
+        Debug.Log("scriptCommand 들어옴");
+        string head = _command.Split('(')[0];
+
+
+        switch(head)
+        {
+            case "스크립트":
+                {
+                    string[] parameter = MakeFuctionForm(_command);
+                    GetDialogue(parameter[0]);
+                    StopCoroutine("TalkCoroutine");
+                    sentenceIndex = 1;
+                    isRunningTalkCoroutine = false;
+                }break;
+
+            case "대사연결":
+                {
+                    isConnectingSentence = true;
+                }
+                break;
+
+            case "대사연결제거":
+                {
+                    isConnectingSentence = false;
+                }
+                break;
+
+
+
+
+
+            case "배경출력":
+                {
+                    string[] parameter = MakeFuctionForm(_command);
+                    if (parameter.Length>1)
+                    {
+                        float changeTime;
+                        float.TryParse(parameter[1] , out changeTime);
+                        SpriteManager.GetInstance().DrawBackGround(parameter[0], changeTime);
+                        return;
+                    }
+                    SpriteManager.GetInstance().DrawBackGround(parameter[0]);
+                }
+                break;
+
+            case "배경제거":
+                {
+                    string[] parameter = MakeFuctionForm(_command);
+                    Debug.Log(parameter.Length);
+
+                    if (parameter[0]!="")
+                    {
+                        float changeTime;
+                        float.TryParse(parameter[0], out changeTime);
+                        SpriteManager.GetInstance().HideBackGround(changeTime);
+                        return;
+                    }
+                        
+                    else
+                    {
+                        SpriteManager.GetInstance().HideBackGround();
+                    }
+
+                }
+                break;
+
+
+            case "캐릭터출력":
+                {
+                    
+                    string[] parameter = MakeFuctionForm(_command);
+
+                    int expression;
+                    int.TryParse(parameter[1], out expression);
+
+                    if(parameter.Length ==3)
+                    {
+                        float drawingSpeed;
+                        float.TryParse(parameter[2], out drawingSpeed);
+                        SpriteManager.GetInstance().DrawCharacter(parameter[0], expression, drawingSpeed);
+                        return;
+                    }
+                    SpriteManager.GetInstance().DrawCharacter(parameter[0], expression);
+
+
+                } break;
+
+            case "캐릭터제거":
+                {
+                    string[] parameter = MakeFuctionForm(_command);
+                    SpriteManager.GetInstance().HideCharacter(parameter[0]);
+                }
+                break;
+
+            case "표정":
+                {
+                    string[] parameter = MakeFuctionForm(_command);
+                    int expressionIndex;
+                    int.TryParse(parameter[1], out expressionIndex);
+                    if(parameter.Length>2)
+                    {
+                        float changeTime;
+                        float.TryParse(parameter[2], out changeTime);
+                        SpriteManager.GetInstance().ChangeExpression(parameter[0], expressionIndex,changeTime);
+                        return;
+                    }
+                    SpriteManager.GetInstance().ChangeExpression(parameter[0], expressionIndex);
+                }
+                break;
+
+            case "배경음":
+                {
+                    string[] parameter = MakeFuctionForm(_command);
+                    SoundManager.GetInstance().PlayBgm(parameter[0]);
+                }
+                break;
+
+            case "배경음제거":
+                {
+                    SoundManager.GetInstance().StopBgm();
+                }break;
+
+            case "효과음":
+                {
+                    string[] parameter = MakeFuctionForm(_command);
+                    SoundManager.GetInstance().PlayEffect(parameter[0]);
+                }
+                break;
+
+            case "효과음제거":
+                {
+                    SoundManager.GetInstance().StopEffect();
+                }
+                break;
+
+
+            default:
+                {
+                    Debug.Log("명령어를 잘못 입력했습니다.");
+                }break;
+        }
+    }
+
+    string[] MakeFuctionForm(string _command)
+    {    
+        string[] s = _command.Split(new char[] { '(',')' });
+        string[] parameter = s[1].Split('/');
+
+        //공백제거
+        for(int i=0; i<parameter.Length; i++)
+        {
+            parameter[i].Trim();
+            Debug.Log("parameter"+i+" : " + parameter[i]);
+        }
+
+        
+        return parameter;
+
+    }
+
+
+               
+    void SentenceInitializer()
+    {
+        sentence.Clear();
+        sentenceIndex       = 1;
+        nameText.text       = "";
+        dialogueText.text   = "";
+    }
+
+    #endregion
+
+
+  
+
+
+    void Start()
+    {
+        GetDialogue("script1");
+
+    }
+
+    void Update()
+    {
+        //타이핑이 되고 있거나, 그려지고 있으면 클릭이 불가.
+        if(Input.GetKeyDown(KeyCode.A) && isRunningTalkCoroutine==false && SpriteManager.GetInstance().isRunningDrawCoroutine()==false)
+        {
+            Talk();
+        }
+        
+
+    }
+
+
+
+
+
+}
+
+
---
+++ SpriteManager.cs
@@ -0,0 +1,337 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteManager : MonoBehaviour
+{
+    private static SpriteManager instance;
+    public static SpriteManager GetInstance()
+    {
+        if(instance ==null)
+        {
+            instance = FindObjectOfType<SpriteManager>();
+            if(instance==null)
+            {
+                GameObject container = new GameObject("SpriteManager");
+                instance =container.AddComponent<SpriteManager>();
+            }
+        }
+        return instance;
+    }
+
+
+    //UI
+    public Image backGroundImage;
+    public Image characterPrefab;    
+
+
+
+    //캐릭터 스프라이트
+    public Sprite[] body;
+    public Sprite[] jisooExpression;
+    public Sprite[] hannahExpression;
+    public Sprite[] jieunExpression;
+
+
+
+
+
+    //코루틴 순서용 변수
+    bool isHidingBackGround = false;
+    bool isDrawingBackGround = false;
+    [HideInInspector]
+    public bool isDrawingCharacter = false;
+    bool isChangingExpression = false;
+    bool isHidingCharacter = false;
+
+    public bool isRunningDrawCoroutine()
+    {
+        if(isHidingBackGround || isDrawingBackGround || isDrawingCharacter || isHidingCharacter)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+
+
+
+    #region 캐릭터 생성 삭제 표정변화 관련
+
+    public void DrawCharacter(string _charName, int _expression, float _targetTime=0.7f)
+    {
+        Image character = Instantiate(characterPrefab, GameObject.Find("Character").transform);
+        character.name = _charName;
+
+        if (_charName.Contains("지유"))
+        {
+            character.sprite = GetBodySprite("지유");
+            GameObject.Find(_charName).transform.GetChild(0).GetComponent<Image>().sprite =
+                GetExpressionSprite("지유", _expression);
+        }
+        else if (_charName.Contains("한나"))
+        {
+            character.sprite = GetBodySprite("한나");
+            GameObject.Find(_charName).transform.GetChild(0).GetComponent<Image>().sprite =
+                GetExpressionSprite("한나", _expression);
+        }
+        else if (_charName.Contains("루리"))
+        {
+            character.sprite = GetBodySprite("루리");
+            GameObject.Find(_charName).transform.GetChild(0).GetComponent<Image>().sprite =
+                GetExpressionSprite("루리", _expression);
+        }
+        else
+        {
+            Debug.Log("스프라이트 로드 에러! 스크립트를 확인해주세요.");
+            return;
+        }
+
+        StartCoroutine(DrawCharacterCoroutine(_charName, _targetTime));
+    }
+    
+
+    IEnumerator DrawCharacterCoroutine(string _gameObject, float _targetTime)
+    {
+        isDrawingCharacter = true;
+        //배경을 그리는 동안 캐릭터가 그려지지 않도록
+        while(isDrawingBackGround)
+        {
+            yield return null;
+        }
+        
+        Image body = GameObject.Find(_gameObject).GetComponent<Image>();
+        Image expression = GameObject.Find(_gameObject).transform.GetChild(0).GetComponent<Image>();
+        if (body == null)
+        {
+            Debug.Log("오브젝트를 찾을 수 없습니다.");
+            yield break;
+        }
+
+        float nowTime = 0;
+        float targetTime = _targetTime;
+        float r = body.GetComponent<Image>().color.r;
+        float g = body.GetComponent<Image>().color.g;
+        float b = body.GetComponent<Image>().color.b;
+        float a;
+        yield return null;
+        while (nowTime < _targetTime)
+        {
+            nowTime += Time.deltaTime;
+            a = Mathf.Lerp(0, 1, nowTime / targetTime);
+            body.color = new Color(r, g, b, a);
+            expression.color = new Color(r, g, b, a);
+            yield return null;
+        }
+        isDrawingCharacter = false;
+        yield return null;
+    }
+
+    public void HideCharacter(string _charName)
+    {
+        StartCoroutine("HideCharacterCoroutine",_charName);
+    }
+
+    IEnumerator HideCharacterCoroutine(string _charName)
+    {
+        isHidingCharacter = true;
+        
+        while(isChangingExpression||isDrawingCharacter)
+        {
+            yield return null;
+        }
+
+        Image characterImage = GameObject.Find(_charName).GetComponent<Image>();
+        Image expressionImage = GameObject.Find(_charName).transform.GetChild(0).GetComponent<Image>();
+        if(characterImage==null)
+        {
+            Debug.Log("캐릭터가 생성되지 않았거나, 잘못된 이름을 입력했습니다");
+            yield break;
+        }
+        float nowTime = 0;
+        float targetTime = 0.5f;
+        float a;
+
+        while(nowTime<targetTime)
+        {
+            nowTime += Time.deltaTime;
+            a = Mathf.Lerp(1, 0, nowTime / targetTime);
+            characterImage.color = new Color(1, 1, 1, a);
+            expressionImage.color = new Color(1, 1, 1, a);
+            yield return null;
+        }
+        yield return null;
+        isHidingCharacter = false;
+        Destroy(GameObject.Find(_charName));
+    }
+    
+    public void ChangeExpression(string _gameObject, int expressionIndex, float _targetTime = 0.7f)
+    {
+        StartCoroutine(ChangeExpressionCoroutine(_gameObject, expressionIndex, _targetTime));
+    }
+    IEnumerator ChangeExpressionCoroutine(string _gameObject, int expressionIndex, float _targetTime)
+    {
+        isChangingExpression = true;
+        while(isDrawingCharacter)
+        {
+            yield return null;
+        }
+        float nowTime = 0;
+        float targetTime = _targetTime;
+        Image expression = GameObject.Find(_gameObject).transform.GetChild(0).GetComponent<Image>();
+        Image newExpression = GameObject.Find(_gameObject).transform.GetChild(1).GetComponent<Image>();
+        newExpression.sprite = GetExpressionSprite(_gameObject, expressionIndex);
+        float a;
+        float aa;
+
+        while (nowTime < targetTime)
+        {
+            nowTime += Time.deltaTime;
+            a = Mathf.Lerp(1, 0, nowTime / targetTime);
+            aa = Mathf.Lerp(0.6f, 1, nowTime / targetTime);
+            expression.color = new Color(1, 1, 1, a);
+            newExpression.color = new Color(1, 1, 1, aa);
+            yield return null;
+        }
+        expression.sprite = newExpression.sprite;
+        expression.color = new Color(1, 1, 1, 1);
+        newExpression.color = new Color(1, 1, 1, 0);
+        yield return null;
+        isChangingExpression = false;
+
+    }
+
+
+
+    Sprite GetBodySprite(string _charName)
+    {
+        if (_charName.Contains("지유"))
+        {
+            return SpriteManager.GetInstance().body[0];
+        }
+        else if (_charName.Contains("한나"))
+        {
+            return SpriteManager.GetInstance().body[1];
+        }
+        else if (_charName.Contains("루리"))
+        {
+            return SpriteManager.GetInstance().body[2];
+        }
+        else
+        {
+            Debug.Log("스프라이트 로드 에러! 스크립트를 확인해주세요.");
+            return null;
+        }
+    }
+
+    Sprite GetExpressionSprite(string _charName, int _expressionIndex)
+    {
+        if (_charName.Contains("지유"))
+        {
+            return SpriteManager.GetInstance().jisooExpression[_expressionIndex];
+        }
+
+        else if (_charName.Contains("한나"))
+        {
+            return SpriteManager.GetInstance().hannahExpression[_expressionIndex];
+        }
+        else if (_charName.Contains("루리"))
+        {
+            return SpriteManager.GetInstance().jieunExpression[_expressionIndex];
+        }
+        else
+        {
+            Debug.Log("스프라이트 로드 에러! 스크립트를 확인해주세요.");
+            return null;
+        }
+    }
+
+
+    #endregion
+
+    #region 배경 생성 삭제
+
+    public void DrawBackGround(string _path, float _targetTime = 0.75f)
+    {
+        StartCoroutine(DrawBackGroundCoroutine(_path, _targetTime));
+    }
+
+    IEnumerator DrawBackGroundCoroutine(string _path, float _targetTime)
+    {
+        while(isHidingBackGround==true)
+        {
+            yield return null;
+        }
+
+        isDrawingBackGround = true;
+
+        DialogueManager.GetInstance().OpenDialogueBox(false);
+        yield return new WaitForEndOfFrame();
+
+        Sprite sprite = GetBackGroundSprite(_path);
+        yield return null;
+        backGroundImage.sprite = sprite;
+        float alpha;
+        float nowTime = 0;
+        float targetTime = _targetTime;
+        while(nowTime<targetTime)
+        {
+            nowTime += Time.deltaTime;
+            alpha = Mathf.Lerp(0, 1, nowTime / targetTime);
+            backGroundImage.color = new Color(1, 1, 1, alpha);
+            yield return null;
+        }
+        isDrawingBackGround = false;
+        yield return new WaitForSeconds(0.5f);
+        DialogueManager.GetInstance().OpenDialogueBox(true);
+
+    }
+
+    public void HideBackGround(float _targetTime = 1.0f)
+    {
+        
+        StartCoroutine(HideBackGroundCoroutine(_targetTime));
+    }
+
+    IEnumerator HideBackGroundCoroutine(float _targetTime)
+    {
+        isHidingBackGround = true;
+
+        float nowTime = 0;
+        float targetTime = _targetTime;
+        float alpha;
+        DialogueManager.GetInstance().OpenDialogueBox(false);
+        yield return null;
+
+        while(nowTime<targetTime)
+        {
+            nowTime += Time.deltaTime;
+            alpha = Mathf.Lerp(1, 0, nowTime / targetTime);
+            backGroundImage.color = new Color(1, 1, 1, alpha);
+            yield return null;
+        }
+        yield return null;
+        isHidingBackGround = false;
+    }
+
+    Sprite GetBackGroundSprite(string _path)
+    {
+        Sprite sprite;
+        sprite = Resources.Load<Sprite>("BackGround/" + _path);
+        if(sprite!=null)
+        {
+            return sprite;
+        }
+        else
+        {
+            Debug.Log("배경 스프라이트가 로드되지 않았습니다");
+            return null;
+        }
+    }
+
+    #endregion
+}
